378071114c61e06ec3d5fd32fa02598f989d4fc3
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-01-01 07:01:14 +0530

    Add an optimised approach to Assign Cookie problem

---
+++ 001-1st_jan-Assign_Cookies/code.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int count = 0;
+        int i = 0;
+        int j = 0;
+        while (i < g.size() && j < s.size()) {
+            if (s[j] >= g[i]) {
+                count++;
+                i++;
+            }
+            j++;
+        }
+        return count;
+    }
+};

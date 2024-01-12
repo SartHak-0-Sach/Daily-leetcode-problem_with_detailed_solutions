f19bf03b6829f1aa0ad941144c44e49517e8c4b2
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-01-12 12:19:36 +0530

    add optimized O(N) solution of string problem

---
+++ 012-12th_jan-Half_strings_alike/code.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    bool halvesAreAlike(string s) {
+        auto countVowels = [](const string& str) {
+            unordered_set<char> vowels{'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+            int count = 0;
+            for (char c : str) {
+                if (vowels.count(c) > 0) {
+                    count++;
+                }
+            }
+            return count;
+        };
+
+        int length = s.length();
+        int midPoint = length / 2;
+
+        string firstHalf = s.substr(0, midPoint);
+        string secondHalf = s.substr(midPoint);
+
+        return countVowels(firstHalf) == countVowels(secondHalf);
+    }
+};

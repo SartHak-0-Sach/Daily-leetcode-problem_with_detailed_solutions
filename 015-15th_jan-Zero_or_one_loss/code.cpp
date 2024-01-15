292e4d18c13772185afa9b6c11a6aee56b41f07e
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-01-15 07:06:47 +0530

    Solve daily question using sets

---
+++ 015-15th_jan-Zero_or_one_loss/code.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+        unordered_set<int> zeroLoss, oneLoss, moreLoss;
+
+        for (auto& match : matches) {
+            int winner = match[0], loser = match[1];
+            if ((oneLoss.find(winner) == oneLoss.end()) &&
+                (moreLoss.find(winner) == moreLoss.end())) {
+                zeroLoss.insert(winner);
+            }
+            if (zeroLoss.find(loser) != zeroLoss.end()) {
+                zeroLoss.erase(loser);
+                oneLoss.insert(loser);
+            } else if (oneLoss.find(loser) != oneLoss.end()) {
+                oneLoss.erase(loser);
+                moreLoss.insert(loser);
+            } else if (moreLoss.find(loser) != moreLoss.end()) {
+                continue;
+            } else {
+                oneLoss.insert(loser);
+            }
+        }
+
+        vector<vector<int>> answer(2, vector<int>());
+        answer[0].assign(zeroLoss.begin(), zeroLoss.end());
+        answer[1].assign(oneLoss.begin(), oneLoss.end());
+        
+        sort(answer[0].begin(), answer[0].end());
+        sort(answer[1].begin(), answer[1].end());
+
+        return answer;
+    }
+};

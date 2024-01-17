925f4be825380410c53dcd020746edbff62fc851
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-01-17 13:12:44 +0530

    Add daily problem solution and approach

---
+++ 017-17th_jan-unique_num_occurence/code.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+      unordered_map<int,int>freq;
+      for(auto x: arr){
+          freq[x]++;
+      }
+      unordered_set<int>s;
+      for(auto x: freq){
+          s.insert(x.second);
+      }
+      return freq.size()==s.size();
+    }
+};

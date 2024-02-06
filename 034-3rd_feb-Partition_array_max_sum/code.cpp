9f02e886fa5488764a7d1eb86f6288b74c64cf74
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-02-06 09:58:15 +0530

    Add 3rd feb daily problem optimised code

---
+++ 034-3rd_feb-Partition_array_max_sum/code.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        int N = arr.size();
+        int K = k + 1;
+
+        vector<int> dp(K);
+
+        for (int start = N - 1; start >= 0; start--) {
+            int currMax = 0;
+            int end = min(N, start + k);
+            
+            for (int i = start; i < end; i++) {
+                currMax = max(currMax, arr[i]);
+                dp[start % K] = max(dp[start % K], dp[(i + 1) % K] + currMax * (i - start + 1));
+            }
+        }
+        return dp[0];
+    }
+};

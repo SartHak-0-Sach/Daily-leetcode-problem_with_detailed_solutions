3b1b40b79fe5ef578e9ae848a5e64d786ae44e96
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-02-08 10:00:05 +0530

    Used memoization to solve 8th feb problem

---
+++ 039-8th_feb-Perfect_squares/code.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <climits>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int numSquares(int n)
+    {
+        vector<int> dp(n + 1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 1; i <= n; ++i)
+        {
+            for (int j = 1; j * j <= i; ++j)
+            {
+                dp[i] = min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
+    }
+};

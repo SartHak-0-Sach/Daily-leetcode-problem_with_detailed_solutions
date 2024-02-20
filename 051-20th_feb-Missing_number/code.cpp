4551ca7218ce05e04f104c558161dcce26c506a2
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-02-20 22:46:49 +0530

    Add daily problem solution

---
+++ 051-20th_feb-Missing_number/code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>v(n+1,-1);
+        for(int i =0;i<nums.size();i++){
+            v[nums[i]] = nums[i];
+        }
+        for(int i =0;i<v.size();i++){
+            if(v[i]==-1)return i;
+        }
+        return 0;
+    }
+};

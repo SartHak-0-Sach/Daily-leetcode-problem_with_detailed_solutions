09a3f877506924204b600e6f976469d8e062335a
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-02-21 10:52:41 +0530

    Solve 21st Feb problem in O(logN) time complexity

---
+++ 052-21st_feb-Bitwise_AND_number_range/code.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        int cnt = 0;
+        while (left != right) {
+            left >>= 1;
+            right >>= 1;
+            cnt++;
+        }
+        return (left << cnt);
+    }
+};

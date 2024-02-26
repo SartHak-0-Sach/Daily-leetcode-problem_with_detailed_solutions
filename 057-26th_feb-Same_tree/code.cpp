923da7d655dfdb6748551784dea78014eec76049
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-02-26 07:53:38 +0530

    Solve easy trees daily problem

---
+++ 057-26th_feb-Same_tree/code.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class TreeNode
+{
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(!p && !q)    return true;
+        if(!p || !q)    return false;
+        return p->val==q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};

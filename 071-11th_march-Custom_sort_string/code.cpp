c27e93e368fe3d16470a33bad84239181f5f2691
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-03-12 04:17:36 +0530

    Create code.cpp

---
+++ 071-11th_march-Custom_sort_string/code.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+    string customSortString(string order, string s)
+    {
+        string result = "";
+        unordered_map<int, int> mp;
+        for (auto &it : s)
+        {
+            mp[it]++;
+        }
+        for (auto &it : order)
+        {
+            if (mp.find(it) != mp.end())
+                result.append(mp[it], it);
+            mp.erase(it);
+        }
+        for (auto &it : mp)
+        {
+            result.append(it.second, it.first);
+        }
+        return result;
+    }
+};

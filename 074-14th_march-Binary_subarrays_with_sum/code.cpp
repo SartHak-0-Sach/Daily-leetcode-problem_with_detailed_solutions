bb4d051924862e5bda676b6e871d10b26a92b7bd
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-03-14 19:10:15 +0530

    Create code.cpp

---
+++ 074-14th_march-Binary_subarrays_with_sum/code.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int numSubarraysWithSum(vector<int> &nums, int goal)
+    {
+        int i = 0, count = 0, res = 0;
+        for (int j = 0; j < nums.size(); ++j)
+        {
+            if (nums[j] == 1)
+            {
+                goal--;
+                count = 0;
+            }
+
+            while (goal == 0 && i <= j)
+            {
+                goal += nums[i];
+                i++;
+                count++;
+                if (i > j - goal + 1)
+                    break;
+            }
+
+            while (goal < 0)
+            {
+                goal += nums[i];
+                i++;
+            }
+
+            res += count;
+        }
+        return res;
+    }
+};

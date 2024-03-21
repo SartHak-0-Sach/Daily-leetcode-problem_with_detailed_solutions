a600b5dead46c6c38d08ec4f2d786f2348a207a7
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-03-21 15:08:38 +0530

    Solve easy daily linked list question

---
+++ 081-21st_march-Reverse_linked-list/code.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class ListNode
+{
+public:
+    int val;
+    ListNode *next;
+    ListNode(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        ListNode *nextNode, *prevNode = NULL;
+        while (head)
+        {
+            nextNode = head->next;
+            head->next = prevNode;
+            prevNode = head;
+            head = nextNode;
+        }
+        return prevNode;
+    }
+};

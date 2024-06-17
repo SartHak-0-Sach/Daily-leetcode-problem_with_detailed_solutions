fb4c6c321f33eea8f14fc640b275d22b96566da9
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-06-17 12:17:38 +0530

    Commit daily problem solution

---
+++ 168-16th_June-Patching_array/code.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <climits>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long long miss = 1;
+        int result = 0;
+        size_t i = 0;
+
+        while (miss <= n) {
+            if (i < nums.size() && nums[i] <= miss) {
+                miss += nums[i];
+                i++;
+            } else {
+                miss += miss;
+                result++;
+            }
+        }
+
+        return result;
+    }
+};

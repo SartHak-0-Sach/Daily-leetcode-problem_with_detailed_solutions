2e2b573534b09add547d0065d7994d87c28b513a
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-06-18 23:55:53 +0530

    Solve daily problem and add solution

---
+++ 170-18th_June-Assigning_work/code.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        int maxDifficulty = *max_element(difficulty.begin(), difficulty.end());
+        vector<int> maxProfitUpToDifficulty(maxDifficulty + 1, 0);
+
+        for (int i = 0; i < difficulty.size(); ++i) {
+            maxProfitUpToDifficulty[difficulty[i]] = max(maxProfitUpToDifficulty[difficulty[i]], profit[i]);
+        }
+
+        for (int i = 1; i <= maxDifficulty; ++i) {
+            maxProfitUpToDifficulty[i] = max(maxProfitUpToDifficulty[i], maxProfitUpToDifficulty[i - 1]);
+        }
+
+        int totalProfit = 0;
+        for (int ability : worker) {
+            if (ability > maxDifficulty) {
+                totalProfit += maxProfitUpToDifficulty[maxDifficulty];
+            } else {
+                totalProfit += maxProfitUpToDifficulty[ability];
+            }
+        }
+
+        return totalProfit;
+    }
+};

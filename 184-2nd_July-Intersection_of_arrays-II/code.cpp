ceb2ee54f4be4554e3a8fd6b49f5f651c0eca813
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-10-25 12:29:48 +0530

    Update optimized approach code

--- 184-2nd_July-Intersection_of_arrays-II/code.cpp
+++ 184-2nd_July-Intersection_of_arrays-II/code.cpp
@@ -1,24 +1,43 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
-#include <algorithm>
 using namespace std;
-
-class Solution {
+#define nline '\n'
+#define sp ' '
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+static int fastIO = []
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
+// Paste the Class here-
+class Solution
+{
 public:
-    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> res;
-        unordered_map<int, int> box;
-        for (int num : nums1)
-            box[num]++;
-        for (int i = 0; i < nums2.size(); ++i) {
-            if (box[nums2[i]] > 0) {
-                res.push_back(nums2[i]);
-                box[nums2[i]]--;
-            } else {
-                continue;
+    vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
+    {
+        unordered_map<int, int> hashmap;
+        vector<int> Res;
+        for (int i = 0; i < nums1.size(); i++)
+        {
+            hashmap[nums1[i]]++;
+        }
+        for (int i = 0; i < nums2.size(); i++)
+        {
+            int element = nums2[i];
+            if (hashmap.find(element) != hashmap.end())
+            {
+                Res.push_back(element);
+                hashmap[element]--;
+                if (hashmap[element] == 0)
+                {
+                    hashmap.erase(element);
+                }
             }
         }
-        return res;
+        return Res;
     }
 };
